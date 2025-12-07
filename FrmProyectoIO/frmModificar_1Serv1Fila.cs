4a22075710fadc36c3c3cc1dd8dcc570b4b8c674
Angel Zavala <angelobedsalazar@gmail.com>
2025-12-07 16:36:09 -0600

    Agregado: frmModificar_1Serv1Fila (sin diseño)

---
+++ FrmProyectoIO/frmModificar_1Serv1Fila.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmProyectoIO
+{
+    public partial class frmModificar_1Serv1Fila : Form
+    {
+        public frmModificar_1Serv1Fila()
+        {
+            InitializeComponent();
+        }
+    }
+}
