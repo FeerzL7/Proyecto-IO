3af3a350aca2cb339266da06ca91d740f370fc8e
abelsalazar-14 <abelsalazar0909@gmail.com>
2025-12-08 08:32:27 -0600

    Clases agregadas

---
+++ FrmProyectoIO/Inventario.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrmProyectoIO
+{
+    public enum Dificultad
+    {
+        Facil, Dificil
+    }
+    public class Inventario
+    {
+        public string Texto { get; set; } = "";
+        public ushort D { get; set; }
+        public decimal Co { get; set; }
+        public decimal Ch { get; set; }
+        public ushort L { get; set; }
+        public ushort Y { get; set; }
+        public ushort d { get; set; }
+
+        public virtual double Q
+        {
+            get
+            {
+                double Operacion = Math.Sqrt((double)((2 * D * Co) / Ch));
+                return Operacion;
+            }
+        }
+
+        public decimal CAO
+        {
+            get { return ((decimal)Q / 2) * Ch; }
+        }
+        public decimal CAA
+        {
+            get { return (D / (decimal)Q) * Co; }
+        }
+        public virtual decimal CT
+        {
+            get { return CAA + CAO; }
+        }
+        public double To
+        {
+            get
+            {
+
+                if (Y > 0)
+                {
+                    double d = ((Q / D) * Y);
+                    return d;
+
+                }
+                return (Q / D);
+            }
+        }
+
+        public double CantidadPromInventario
+        {
+            get { return (ushort)(Q / 2); }
+        }
+
+        public double Le
+        {
+            get
+            {  //L siempre esta en dias
+                if (Y == 0)
+                {
+                    return 0;
+                }
+                if (L > To)
+                {
+                    int n = (int)(L / To);
+                    return L - (n * To);
+                }
+                return L;
+            }
+        }
+
+        public double R
+        {
+
+            get
+            {
+                if (d == 0)
+                {
+                    return 0;
+                }
+                return (Le * d);
+            }
+        }
+
+    }
+}
---
+++ FrmProyectoIO/InventarioProduccion.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrmProyectoIO
+{
+    public class InventarioProduccion : Inventario
+    {
+        public ushort p { get; set; }
+
+        public override double Q
+        {
+            get
+            {
+                double factor = 1 - ((double)d / p);
+                return Math.Sqrt((double)(2 * D * Co) / ((double)Ch * factor));
+            }
+        }
+
+
+        public double N
+        {
+            get { return (D / Q); }
+        }
+
+        public decimal Cprep
+        {
+            get { return (decimal)(D / Q) * Co; }
+        }
+        public decimal Calm
+        {
+            get
+            {
+                double factor = 1 - ((double)d / p);
+
+                return (decimal)(Q / 2) * Ch * (decimal)factor;
+            }
+        }
+
+        public override decimal CT
+        {
+            get
+            {
+                return Cprep + Calm;
+                ;
+            }
+        }
+        public double Tp
+        {
+            get { return Q / p; }
+        }
+
+        public double Imax
+        {
+            get
+            {
+                decimal factor = 1 - ((decimal)d / p);
+
+                return (Q * (double)factor);
+            }
+        }
+
+        public double Iprom
+        {
+            get
+            {
+                decimal factor = 1 - ((decimal)d / p);
+
+                return (Q / 2) * (double)factor;
+            }
+        }
+    }
+}
